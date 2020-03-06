15bfcb4b9f98e779b52a0fdcf3fb3abfa2803c58
ibrahim-kamagate <ibrahim.kamagate@ms223.org>
2020-03-05 21:28:22 -0500

    idk

---
+++ Assets/Scenes/scripts/class2 2.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+//
+public class class2 : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject endObject;
+    public Transform[] hazzard;
+    public int i;
+
+    
+
+    AudioSource die;
+    public AudioClip HazzardClip;
+
+    AudioSource move;
+    public AudioClip MoveClip;
+
+    AudioSource end;
+    public AudioClip EndClip;
+
+    Vector3 initPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = player.transform.position;
+
+        die = GetComponent<AudioSource>();
+        move = GetComponent<AudioSource>();
+        end = GetComponent<AudioSource>();
+
+
+    }
+
+    // Update is called once per frameXDSA
+    void Update()
+    {
+
+
+        //to go left 
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+
+            player.transform.position += new Vector3(0, 0, 1);
+                move.PlayOneShot(MoveClip);
+         
+        }
+        //to go right
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            
+            player.transform.position += new Vector3(0, 0, -1);
+
+                move.PlayOneShot(MoveClip);
+            
+
+        }
+        //to go forward
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+
+            player.transform.position += new Vector3(1, 0, 0);
+            move.PlayOneShot(MoveClip);
+
+        }
+        //to go backward
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+
+            player.transform.position += new Vector3(-1, 0, 0);
+            move.PlayOneShot(MoveClip);
+
+        }
+        //to go up
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+            player.transform.position += new Vector3(0, 1, 0);
+            move.PlayOneShot(MoveClip);
+
+        }
+        //to go down
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+            player.transform.position += new Vector3(0, -1, 0);
+            move.PlayOneShot(MoveClip);
+
+        }
+
+        //loop to check 
+        for (int i = 0; i < hazzard.Length; i++)
+        {
+            // negitive affect 
+            if (hazzard[i].position == player.transform.position)
+            {
+                player.transform.position = initPos;
+                die.PlayOneShot(HazzardClip);
+                SceneManager.LoadScene("lose");
+
+
+
+            }
+        }
+
+
+        if (player.transform.position == endObject.transform.position)
+        {
+            end.PlayOneShot(EndClip);
+            SceneManager.LoadScene("Win");
+        }
+    }
+
+
+
+}
+    
---
+++ Assets/Scenes/scripts/ui 2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ui : MonoBehaviour
+{
+    public Text playerMsg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            
+            playerMsg.text = "Resume";
+            playerMsg.text = "Restart";
+            playerMsg.text = "Quit";
+
+        }
+    }
+}
